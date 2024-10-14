8f243b89cd5f811853fc3b44f0b2a29897951306
cromewar <vasiliy@cromewar.com>
2024-10-14 11:42:30 -0500

    adding interface for beforeIncrement and core contract Function

--- src/CounterCore.sol
+++ src/CounterCore.sol
@@ -2,18 +2,44 @@
 pragma solidity ^0.8.20;
 
 import {Core} from "@thirdweb-dev/src/Core.sol";
-
-interface BeforeIncrementCallback {
-    function beforeIncrement(uint256 count) external returns (uint256);
-}
+import {BeforeIncrementCallback} from "./interface/BeforeIncrementCallback.sol";
 
 contract CounterCore is Core {
+    // Contract variables
+    uint256 count;
+
+    // Constructor
+    constructor(address owner) {
+        _initializeOwner(owner);
+    }
+
+    // @dev - add this function to avoid warnings.
     receive() external payable {}
 
+    // Function required by the Core contract import
     function getSupportedCallbackFunctions()
         public
         pure
         override
         returns (SupportedCallbackFunction[] memory supportedCallbackFunctions)
     {}
+
+    // Contract function
+
+    function increment() public {
+        count += 1;
+    }
+
+    // Interface Functions
+
+    function _beforeIncrement(
+        uint256 _count
+    ) internal returns (uint256 newCount) {
+        (, bytes memory returnData) = _executeCallbackFunction(
+            BeforeIncrementCallback.beforeIncrement.selector,
+            abi.encodeCall(BeforeIncrementCallback.beforeIncrement, (_count))
+        );
+
+        newCount = abi.decode(returnData, (uint256));
+    }
 }
