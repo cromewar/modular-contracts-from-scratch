811529a3d118ed39a0216eb4f6216cdeee130fd3
cromewar <vasiliy@cromewar.com>
2024-10-14 16:22:24 -0500

    adding the fallback function

--- src/CounterModule.sol
+++ src/CounterModule.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.20;
 
 import {Module} from "@thirdweb-dev/src/Module.sol";
+import {CounterStorage} from "./library/CounterStorage.sol";
 
 contract CounterModule is Module {
     // Functions required by the Module import
@@ -12,4 +13,24 @@ contract CounterModule is Module {
         override
         returns (ModuleConfig memory config)
     {}
+
+    // Contract Function
+
+    // Counter Storage uses the the library and the Data struct.
+    function _counterStorage()
+        internal
+        pure
+        returns (CounterStorage.Data storage)
+    {
+        return CounterStorage.data();
+    }
+
+    // Callback and Fallback Functions
+    function getStep() external view returns (uint256) {
+        return CounterStorage.data().step;
+    }
+
+    function setStep(uint256 _step) external {
+        CounterStorage.data().step = _step;
+    }
 }
